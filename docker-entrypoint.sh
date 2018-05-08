e75400cf1cdba77474c9ecd313ed83db83f9ba67
MetalArend <metalarend@gmail.com>
2018-05-09 00:22:50 +0200

    complete rewrite to use with tags on dockercloud

---
+++ docker-entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+isCommand() {
+  for cmd in \
+    "analyse" \
+    "help" \
+    "list"
+  do
+    if [ -z "${cmd#"$1"}" ]; then
+      return 0
+    fi
+  done
+
+  return 1
+}
+
+if [ "${1:0:1}" = "-" ]; then
+  set -- /sbin/tini -- php /vendor/bin/php-cs-fixer "$@"
+elif [ "$1" = "/vendor/bin/php-cs-fixer" ]; then
+  set -- /sbin/tini -- php "$@"
+elif [ "$1" = "php-cs-fixer" ]; then
+  set -- /sbin/tini -- php /vendor/bin/"$@"
+elif isCommand "$1"; then
+  set -- /sbin/tini -- php /vendor/bin/php-cs-fixer "$@"
+fi
+
+exec "$@"
